09d903fff92a2b19ea6b7e60d5ac767d7e501d9e
Nikhil-Fulzele <nikhilf99@gmail.com>
2020-02-10 12:06:22 +0700

    setup.py now read requirements from requirements.txt

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ for d, _, _ in os.walk('swag'):
     if 'venv' not in d and os.path.exists(os.path.join(d, '__init__.py')):
         packages.append(d.replace(os.path.sep, '.'))
 
+requirements = []
+with open("requirements.txt", "r") as f:
+    for line in f:
+        requirements.append(line.strip())
 
 setup(
     name="MLSwag",
@@ -15,10 +19,5 @@ setup(
     description="A simple wrapper around ml-model tools to track the parameters, training time and metrics.",
     license="MIT",
     packages = packages,
-    install_requires=[
-        "elasticsearch",
-        "scikit-learn",
-        "pandas",
-        "numpy"
-    ]
+    install_requires=requirements
 )
